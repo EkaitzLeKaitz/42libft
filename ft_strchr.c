9f0846d715e91e96e4ae9b3d14050021f1619130
EkaitzLeKaitz <sagonzal@Santi>
2024-02-10 13:03:41 +0100

    subir ft_strchr.c !=header !=cc !=norminette

---
+++ ft_strchr.c
@@ -0,0 +1,36 @@
+/* strchr => Busca un caracter dentro de una cadena, 
+recorriendola caracter a caracter. 
+Si no lo encuentra devuelve nulo.
+Si lo encuentra devuelve un puntero en la posicion donde encuentra el caracter.
+Si el caracter es nulo devuelve el caracter nulo que encuentre.
+*/
+//#include <string.h>
+//#include <stdio.h>
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != c)
+			i++;
+		else
+			return ((char *)str[i]);
+	}
+	if (c == '\0')
+		return ((char *)str[i]);
+	return (NULL);
+}
+/*
+int	main
+{
+	const char	*str;
+	int		c;
+
+	str = "pepito";
+	c = 'i';
+	printf ("original: %s\n", strchr(str, c));
+//	printf ("propia: %s\n", ft_strchr(str, c));
+	return (0);
+}*/
