e2b05581ae3294fe13fa0e330b4230f18327df70
EkaitzLeKaitz <sagonzal@Santi>
2024-02-13 19:53:33 +0100

    ft_strrchr.c acabada con libreria libft.h

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,9 +1,44 @@
-/* strrchr => (String Reverse Character) Busca un caracter dentro de una 
- cadena, recorriendola caracter a caracter empezando por el final.
+/* 
+  strrchr => (String Reverse Character) Busca un caracter dentro de una 
+  cadena, recorriendola caracter a caracter empezando por el final.
   -Si no lo encuentra devuelve nulo.
   -Si lo encuentra devuelve un puntero en la posicion donde 
   encuentra el caracter.
   -Si el caracter es nulo devuelve el caracter nulo que encuentre. 
-  -Si la cadena introducida esta vacía devuelve NULL return (0) 
-  o return((void *) 0)].
+  -Si str esta vacía devuelve NULL {return (0) o return ((void *) 0)}.
 */
+//#include <string.h>
+//#include <stdio.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(str);
+	if (str == "")
+		return ((void*) 0);
+	if (c == '\0')
+		return ((char*)&str[len]);
+	while (len >= 0)
+	{
+		if (str[len] != c)
+			len--;
+		else
+			return ((char *)&str[len]);
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	const char	*str;
+	int		c;
+
+	c = 'o';
+	str ="comion";
+//	printf ("Original %s\n", strrchr(str, c));
+	printf ("Propia %s\n", ft_strrchr(str, c));
+	return (0);
+}
+ */
