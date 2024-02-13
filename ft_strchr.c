d637582608ccb7bdfb7eb017f2ad7bdddf3c83bb
EkaitzLeKaitz <sagonzal@Santi>
2024-02-13 18:54:47 +0100

    crear ft_strrchr.c y corregir comentario ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,8 +1,10 @@
 /* strchr => (String Character) Busca un caracter dentro de una cadena, 
-recorriendola caracter a caracter. 
-Si no lo encuentra devuelve nulo.
-Si lo encuentra devuelve un puntero en la posicion donde encuentra el caracter.
-Si el caracter es nulo devuelve el caracter nulo que encuentre [0 o ((void *) 0))] .
+ recorriendola caracter a caracter. 
+ -Si no lo encuentra devuelve nulo.
+ -Si lo encuentra devuelve un puntero en la posicion donde encuentra 
+  el caracter.
+ -Si el caracter es nulo devuelve el caracter nulo que encuentre.
+ -Si str esta vacia devuelve NULL {return (0) o return((void *) 0)}.
 */
 //#include <string.h>
 //#include <stdio.h>
@@ -32,7 +34,7 @@ int	main(void)
 
 	str = "pepito";
 	c = 'i';
-	printf ("original: %s\n", strchr(str, c));
-//	printf ("propia: %s\n", ft_strchr(str, c));
+//	printf ("original: %s\n", strchr(str, c));
+	printf ("propia: %s\n", ft_strchr(str, c));
 	return (0);
 }*/
---
+++ ft_strrchr.c
@@ -0,0 +1,9 @@
+/* strrchr => (String Reverse Character) Busca un caracter dentro de una 
+ cadena, recorriendola caracter a caracter empezando por el final.
+  -Si no lo encuentra devuelve nulo.
+  -Si lo encuentra devuelve un puntero en la posicion donde 
+  encuentra el caracter.
+  -Si el caracter es nulo devuelve el caracter nulo que encuentre. 
+  -Si la cadena introducida esta vacía devuelve NULL return (0) 
+  o return((void *) 0)].
+*/
