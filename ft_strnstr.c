1345975268771aee6bd569f5d5aa779cb06d256e
Santiago Gonzalez Rubio <sagonzal@c1r11s1.42madrid.com>
2024-02-19 09:19:16 +0100

    libft.h

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,8 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:16:31 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/19 09:16:48 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/19 09:18:46 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft"
+#include "libft.h"
