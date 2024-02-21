c5c01d98b87cebe8e3b7f7ecc0bbb4547fe9ba74
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 15:01:48 +0100

    arreglado variables strlen

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:55:18 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/21 13:11:18 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:01:18 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-size_t	ft_strlen(const char *str);
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
