bee739825eb7d928d6eb876682cf70c4e82d5259
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 14:55:28 +0100

    arreglado ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,46 +6,52 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:02:06 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/21 13:19:26 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/21 14:54:48 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 Concatena la cadena de destino(dst) con la cadena fuente(src) sin sobrepasar el
 tamaño del buffer deseado(destsize).
- - Si dest < destsize la cadena se trunca.
- - Devuelve la longitud de la cadena concatenada.
+ - Si dst < dstsize la cadena se trunca.
+- Si dstsize <= la len_dst devuelve len_src + dstsize.
+ - Devuelve la longitud de la cadena concatenada posible.
  - Si destsize es 0 devuelve la longitud de la cadena fuente(src).
+ -
 */
-#include <stdio.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <string.h>
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	len_dst;
 	size_t	len_src;
 	size_t	i;
-	size_t	j;
 
 	len_src = ft_strlen(src);
-	i = len_src;
-	j = 0;
-	while (i < (dstsize - 1))
+	len_dst = ft_strlen(dst);
+	i = 0;
+	if (dstsize <= len_dst)
+		return (dstsize + len_src);
+	while (src[i] != '\0' && len_dst < (dstsize - 1))
 	{
-		dst[i] = src[j];
+		dst[len_dst] = src[i];
+		len_dst++;
 		i++;
 	}
-	return (ft_strlen(dst));
+	return ((len_dst - i) + len_src);
 }
 /*
 int	main(void)
 {
-	char		*dst = "";
-	const char	*src = "";
+	char		dst[13] = "perrrro";
+	const char	*src = " gato";
 	size_t		dstsize;
 
-	dstsize = 15;
-	printf("Original: %lu\n", strlcat(dst, src, dstsize));
+	dstsize = 40;
+//	printf("Original: %lu\n", strlcat(dst, src, dstsize));
 	printf("Libft: %zu\n", ft_strlcat(dst, src, dstsize));
+	printf("%s\n", dst);
 	return (0);
 }*/
