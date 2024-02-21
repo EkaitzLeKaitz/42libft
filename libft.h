e17541ad233fa2c4ba36c30d6642e6f92d399c96
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 14:56:20 +0100

    creado librería

---
+++ libft.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 10:55:18 by sagnzal           #+#    #+#             */
+/*   Updated: 2024/02/21 13:11:18 by sagnzal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ifndef =>
+define =>
+LIBFT_H =>
+<stdlibft.h> =>
+<unistd.h> =>
+endif =>
+*/
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnumm(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	*ft_memset(void *str, int c, size_t n);
+void	ft_bzero(void *str, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+#endif
