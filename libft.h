b12fcdddceb5ff8b46d5593cdffd732ddf94f548
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 15:12:01 +0100

    arreglado variables de ft_memset.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:55:18 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/21 15:01:18 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:11:23 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memset(void *b, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
