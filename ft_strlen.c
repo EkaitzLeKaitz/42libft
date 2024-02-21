e2f121a3322755996b7fe06aa3cfc8e4fa67113e
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 15:00:13 +0100

    arreglado main y variables originales ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,21 +6,24 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:10:52 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/01/22 12:00:47 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/21 14:59:32 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* strlen => Devuelve la longitud de una cadena sin contar el caracter nulo
-final.*/
+/* 
+strlen => Devuelve la longitud de una cadena sin contar el caracter nulo
+final.
+*/
+//#include <string.h>
 //#include <stdio.h>
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
 	size_t	cont;
 
 	cont = 0;
-	while (str[cont] != '\0')
+	while (s[cont] != '\0')
 	{
 		cont++;
 	}
@@ -29,8 +32,9 @@ size_t	ft_strlen(const char *str)
 /*
 int	main(void)
 {
-	const char	*str;
-	str = "prueba";
-	printf ("La longitud es %zu\n", ft_strlen(str));
+	const char	*s;
+	s = "prueba";
+	printf ("Original: La longitud es %zu\n", strlen(s));
+	printf ("Libft: La longitud es %zu\n", ft_strlen(s));
 	return (0);
 }*/
