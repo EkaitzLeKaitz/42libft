c636d7973a945f0039cdbdf56c04e62aeb26e0fd
Santiago Gonzalez Rubio <sagonzal@c1r14s5.42madrid.com>
2024-02-21 15:11:13 +0100

    arreglado variables de ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,15 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 08:53:26 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/19 08:53:46 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:09:53 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- memset => Recibe una cadena de cualquier tipo, un
+ memset => Recibe una espacio de memoria de cualquier tipo, un
  size_t n que indica el numero de carcteres que va a alterar
  memset, y un int c que indica el nuevo valor que tendra 
- cada byte de str. (Al convertir a bytes)
+ cada byte de b. (Al convertir a bytes)
  Convierte la cadena a unsigned char, y la sustituye por c.
  Devuelve la dirección de memoria original ya modificada.
  */
@@ -22,35 +22,35 @@
 //#include <string.h>
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memset(void *b, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*onebyte;
 
 	i = 0;
-	onebyte = str;
-	while (i < n && n != 0 && str != 0)
+	onebyte = b;
+	while (i < n && n != 0 && b != 0)
 	{
 		onebyte[i] = c;
 		i++;
 	}
-	return (str);
+	return (b);
 }
 /*
 int	main(void)
 {
-	char	str[4444];
+	char	b[4];
 	int	c;
 	size_t	n;
 
 	n = 4;
 	c = 1432;
-	str[0] = 'H';
-	str[1] = 'O';
-	str[2] = 'L';
-	str[3] = 'I';
-//	memset(str, c, n);
-	ft_memset(str, c, n);
-	printf("%s\n", str);
+	b[0] = 'H';
+	b[1] = 'O';
+	b[2] = 'L';
+	b[3] = 'I';
+//	memset(b, c, n);
+	ft_memset(b, c, n);
+	printf("%s\n", b);
 	return (0);
 }*/
