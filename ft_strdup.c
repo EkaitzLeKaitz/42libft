a1d10e89eb8af3f78ef05b9158c8b85bce20eab4
Santiago Gonzalez Rubio <sagonzal@c1r12s6.42madrid.com>
2024-02-23 09:37:18 +0100

    subir ft_strdpup.c HEADER

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,8 +6,18 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:24:33 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/19 09:24:50 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/02/23 09:36:46 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+strdup => Recibe una string (s1) y reserva memoria para una copia , 
+realiza la copia de la string (s1) y devuelve un puntero a la copia.
+*/
+#include <string.h>
+#include <stdio.h>
 #include "libft.h"
+
+char	*strdup(const char *s1)
+{
+}
