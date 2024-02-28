17f07ab3f5902ab9a96558955c07dfb7f66f5bf5
EkaitzLeKaitz <sagonzal@Santi>
2024-02-28 10:33:05 +0100

    changed if

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -30,7 +30,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	j = 0;
-	if (needle == '\0')
+	if (needle[i] == '\0')
 		return ((char *) haystack);
 	while (i < len && needle[j] && (i + j) < len)
 	{
