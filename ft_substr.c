23584a95a2fc54986336afea49d6343b300ab5da
EkaitzLeKaitz <sagonzal@Santi>
2024-02-28 10:41:51 +0100

    corregido substr

--- ft_substr.c
+++ ft_substr.c
@@ -10,4 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+substr =>  Reserva un espacio de memoria con malloc y devuelve una substring de
+la string s. La substring comienza desde el indice start y tiene una longitud 
+maxima de len.
+Devuelve la substring resultante o NULL si falla la reserva de memoria.
+*/
+//#include <stdio.h>
 #include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub_s;
+	unsigned int	i;
+
+	i = 0;
+	sub_s = malloc(sizeof(char) * (len + 1));
+	if (!sub_s)
+		return (0);
+	while (s[start] && i <= len)
+	{
+		sub_s[i] = s[start];
+		start++;
+		i++;
+	}
+	return (sub_s);
+}
+/*
+int	main(void)
+{
+	char const	*s;
+	unsigned int	start;
+	size_t		len;
+	char		*sub_s;
+
+
+	s = 	"anemoanemona";
+	start = 5;
+	len = 	7;
+	sub_s = ft_substr(s, start, len);
+	printf("%s\n", sub_s);
+	free(sub_s);
+	return (0);
+}*/
