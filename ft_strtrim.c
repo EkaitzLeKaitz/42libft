df2f238513b2659417e38425163bfc3ea0185c72
EkaitzLeKaitz <sagonzal@Santi>
2024-02-29 20:57:30 +0100

    arreglado ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -25,38 +25,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	i;
 	size_t	j;
 	size_t	n;
-	int	len_set;
 	char	*trim;
 
 	trim = 0;
-	len_set = ft_strlen(set);
 	n = 0;
 	i = 0;
 	j = ft_strlen(s1);
-	while (s1[i]  && len_set >= 0)
-	{
-		if (s1[i] != ft_strchr(set, set[len_set]))
-			len_set--;
-		else
-		{
+	while (ft_strchr(set, s1[i]))
 			i++;
-			len_set = ft_strlen(set);
-		}
-	}
-	while (s1[j - 1] && len_set >= 0)
-	{
-		if (s1[j - 1] != ft_strchr(set, set[len_set]))
-			len_set--;
-		else
-		{
+	while (ft_strchr(set, s1[j - 1]))
 			j--;
-			len_set = ft_strlen(set);
-		}
-	}
-	while (s1[i] <= s1[j])
+
+	trim = malloc(sizeof(char) * ((j - i) + 1));
+	if (!trim)
+		return (0);
+	while (i < j)
 	{
 		trim[n] = s1[i];
 		i++;
+		n++;
 	}
 	return (trim);
 }
