cf297b00f818d095c496c46b629cb0b4ae454b88
EkaitzLeKaitz <sagonzal@Santi>
2024-02-29 12:22:03 +0100

    prueba

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,64 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+strtrim => Elimina todos los caracteres de la string "set" desde principio y 
+desde el final de "s1" hasta encontrar un caracter no perteneciente a "set".
+La string resultante se devuelve con una reserva en malloc.
+Devuelve NULL si falla la memoria.
+*/
+
+#include <stdio.h>
 #include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+	size_t	n;
+	int	len_set;
+	char	*trim;
+
+	trim = 0;
+	len_set = ft_strlen(set);
+	n = 0;
+	i = 0;
+	j = ft_strlen(s1);
+	while (s1[i]  && len_set >= 0)
+	{
+		if (s1[i] != ft_strchr(set, set[len_set]))
+			len_set--;
+		else
+		{
+			i++;
+			len_set = ft_strlen(set);
+		}
+	}
+	while (s1[j - 1] && len_set >= 0)
+	{
+		if (s1[j - 1] != ft_strchr(set, set[len_set]))
+			len_set--;
+		else
+		{
+			j--;
+			len_set = ft_strlen(set);
+		}
+	}
+	while (s1[i] <= s1[j])
+	{
+		trim[n] = s1[i];
+		i++;
+	}
+	return (trim);
+}
+
+int	main(void)
+{
+	char	*s1;
+	char	*set;
+
+	s1 = "122212pacoporras12212212";
+	set = "1212";
+	printf("%s\n", ft_strtrim(s1, set));
+	return (0);
+}
