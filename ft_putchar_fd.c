f569c39f7cb2f7f57492d6c2295fa05c8a3d077e
EkaitzLeKaitz <sagonzal@Santi>
2024-03-10 11:51:49 +0100

    ADD ft_putchar_fd.c comleted

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+putchar_fd => Envia el caracter 'c' al file descriptor especificado.
+Para indicar que archivo es el fd, utilizamos la función open() con el nombre
+del archivo y el tipo de permisos en el main. Al terminar de operar ese fd
+lo cerramos con close(fd).
+<fcntl.h> => Libreria del open y close.
+No devuelve nada.
+*/
+//#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/*
+int	main(void)
+{
+	char	c;
+	int		fd;
+
+	c = 'p';
+	fd = open("PruebaPut.txt", O_RDWR | O_CREAT | O_APPEND);
+	ft_putchar_fd(c, fd);
+	close (fd);
+	return (0);
+}*/
