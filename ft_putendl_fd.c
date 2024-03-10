2ce08134d60ecbd4f9ba9c94a3b1f7c341fbf172
EkaitzLeKaitz <sagonzal@Santi>
2024-03-10 12:46:10 +0100

    ADD ft_putendl_fd.c completed

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+putendl_fd => Envia una string 's' al fd dado seguido de un salto de linea.
+No devuelve nada.
+*/
+//#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
 
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int	main(void)
+{
+	char	*s = "NO ESTOY DE BROMA ";
+	int	fd;
+
+	fd = open("Pruebaput.txt", O_CREAT | O_RDWR | O_APPEND);
+	ft_putendl_fd(s, fd);
+	close (fd);
+	return (0);
+}*/
