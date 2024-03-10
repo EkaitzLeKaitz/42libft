1a2bda02fda8e1998183d61bcf66216cc42e67cf
EkaitzLeKaitz <sagonzal@Santi>
2024-03-10 12:27:42 +0100

    ADD ft_putstr_fd.c completed

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+putstr_fd => Envia la string 's' al descriptor especificado.
+La función no devuelve nada.
+O_APPENED = Escribe a continuación en un archivo en vez de sobreescribirlo.
+O_CREAT = Si no existe lo crea.
+O_RDWR = Permisos de lectura y escritura.
+*/
+//#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	char	*s = "Esto no es un simulacro 2 ";
+	int	fd;
+
+	fd = open("Pruebaput.txt", O_RDWR | O_APPEND | O_CREAT);
+	ft_putstr_fd(s, fd);
+	close (fd);
+	return (0);
+}*/
