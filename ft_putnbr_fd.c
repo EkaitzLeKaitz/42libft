41ef71c84ecf0a12c690612e52b3e5dde0198afd
EkaitzLeKaitz <sagonzal@Santi>
2024-03-14 11:03:50 +0100

    ADD ft_putnbr_fd.c completed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+putnbr_fd => Envia el numero n al fd dado.
+*/
+//#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	res;
+
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write (fd, "-", 1);
+			n = -n;
+		}
+		if (n > 9)
+		{
+			ft_putnbr_fd((n / 10), fd);
+		}
+		res = (n % 10) + 48;
+		write (fd, &res, 1);
+	}
+}
+/*
+int	main(void)
+{
+	int	n;
+	int	fd;
+
+	n = -2147483648;
+	fd = open("PruebaPut.txt", O_RDWR | O_CREAT | O_APPEND, 0666);
+	ft_putnbr_fd(n, fd);
+	close (fd);
+	return (0);
+}*/
