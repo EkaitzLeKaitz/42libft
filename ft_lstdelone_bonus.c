f84aa90217cd08f367f2fbf5417e115c13c613b1
Santiago Gonzalez Rubio <sagonzal@c2r19s6.42madrid.com>
2024-03-21 16:34:42 +0100

    COMPLETED

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 15:08:38 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/03/21 15:08:41 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/03/21 16:33:00 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ apunta a otra función. Si es *(*del) significa que si devuelve un puntero.
 Uso ft_strdup para hacer una copia con memoria del content del nodo, ya que
 solo reservo la memoria del nodo en lstnew.
 */
-#include <stdio.h>
+//#include <stdio.h>
 #include "libft.h"
-
-static void	del(*content)
+/*
+static void	del(void *content)
 {
 	free(content);
 }
-
+*/
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
@@ -34,10 +34,15 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del(lst->content);
 	free(lst);
 }
-
+/*
 int	main(void)
 {
-	t_list	nodo1;
+	t_list	*nodo1;
 
 	nodo1 = ft_lstnew(ft_strdup("hola"));
-}
+	nodo1->next = NULL;
+	printf("%s\n", (char *)(nodo1)->content);
+	ft_lstdelone(nodo1, &del);
+	printf("%s\n", (char *)(nodo1)->content);
+	return (0);
+}*/
