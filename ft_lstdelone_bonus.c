5ae30e054629f6bd902bbf36a545e169da86a038
EkaitzLeKaitz <sagonzal@Santi>
2024-03-21 08:59:18 +0100

    ADD ft_lstdelone_bonus.c

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,22 @@
+/*
+lst_delone => Toma como parametro un nodo lst y libera la memoria del contenido
+utilizando la funcion del, además de liberar el nodo. La memoria de next no
+debe liberarse.
+*/
+#include <stdio.h>
+#include "libft.h"
+/*
+static void	*del(*lst)
+{
+	free(lst);
+}
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+}
+
+int	main(void)
+{
+}*/
