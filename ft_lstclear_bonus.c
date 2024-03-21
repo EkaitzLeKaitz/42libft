a1668528a6d31bfdb84d9a01cb7791e13089cd42
Santiago Gonzalez Rubio <sagonzal@c2r19s6.42madrid.com>
2024-03-21 17:09:51 +0100

    ft_lstclear_bonus.c COMPLETED

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,34 +6,35 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 14:23:49 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/03/21 15:08:03 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/03/21 17:08:57 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ft_lstclear.c => Recibe un puntero al primer nodo de una lista, y los borra en
-bucle hasta el final.
+bucle hasta el final. Iguala el puntero del nodo a NULL al terminar.
 */
-#include <stdio.h>
+//#include <stdio.h>
 #include "libft.h"
-
-static void	del(*content)
+/*
+static void	del(void *content)
 {
 	free(content);
 }
-
+*/
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*alter_nodo;
 
-	while (lst)
+	while (*lst)
 	{
-		alter_nodo = &lst;
-		ft_lstdelone(lst, del(lst), (void *));
-		&lst = alter_nodo->next;
+		alter_nodo = *lst;
+		ft_lstdelone(*lst, del);
+		*lst = alter_nodo->next;
 	}
+	*lst = NULL;
 }
-
+/*
 int	main(void)
 {
 	t_list	*nodo1;
@@ -42,15 +43,19 @@ int	main(void)
 	t_list	*nodo4;
 	t_list	**lst;
 
-	lst = &nodo1;
-	nodo1->content = ft_lstmew(ft_strdup("Habia"));
-	nodo2->content = ft_lstmew(ft_strdup("una vez"));
-	nodo3->content = ft_lstmew(ft_strdup("un barquito"));
-	nodo4->content = ft_lstmew(ft_strdup("chiquitito"));
+	nodo1 = ft_lstnew(ft_strdup("Habia"));
+	nodo2 = ft_lstnew(ft_strdup("una vez"));
+	nodo3 = ft_lstnew(ft_strdup("un barquito"));
+	nodo4 = ft_lstnew(ft_strdup("chiquitito"));
 	nodo1->next = nodo2;
 	nodo2->next = nodo3;
 	nodo3->next = nodo4;
 	nodo4->next = NULL;
-	printf("%s\n%s\n", (char *)(lst)->content, (char *)ft_lstlast(lst)->content);
-	ft_lstclear(lst, del(lst), (void *));
-}
+	lst = &nodo1;
+	printf("%s\n%s\n", (char *)(*lst)->content, (char *)
+	/ENTER_PARANORMINETTE/ft_lstlast(*lst)->content);
+	ft_lstclear(lst, del);
+	if (*lst == NULL)
+		printf("Se ha completado la desinstalación");
+	return (0);
+}*/
