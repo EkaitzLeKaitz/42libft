52486e2a193915592396d558c9c3a8b3b47f1bcd
Santiago Gonzalez Rubio <sagonzal@c3r8s4.42madrid.com>
2024-03-22 10:52:10 +0100

    ft_isascii.c COMPLETED

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,17 +6,29 @@
 /*   By: sagnzal <sagonzal@student.42madrid>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:03:35 by sagnzal           #+#    #+#             */
-/*   Updated: 2024/02/19 08:49:55 by sagnzal          ###   ########.fr       */
+/*   Updated: 2024/03/22 10:51:42 by sagnzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* isascii => Devuelve != 0 si el caractér es ascii, sino devuelve 0.*/
+/* 
+isascii => Devuelve != 0 si el caractér es ascii, sino devuelve 0.
+*/
+//#include <stdio.h>
 #include "libft.h"
 
 int	ft_isascii(int c)
 {
-	if (c > 0 && c <= 127)
+	if (c >= 32 && c <= 127)
 		return (1);
 	else
 		return (0);
 }
+/*
+int	main(void)
+{
+	int	c;
+	
+	c = ' ';
+	printf("%d\n", ft_isascii(c));
+	return (0);
+}*/
