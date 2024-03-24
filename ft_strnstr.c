91980c95ef5726e36a07fa8a51e5ed5e1a55eee8
EkaitzLeKaitz <sagonzal@Santi>
2024-03-24 17:34:35 +0100

    COMPLETED PACO

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -32,6 +32,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	if (needle[i] == '\0')
 		return ((char *) haystack);
+	if (haystack[i] == '\0')
+		return (0);
 	while (i < len && needle[j] && (i + j) < len)
 	{
 		if (haystack[i + j] == needle[j])
