9bf9ef2d8c937b909a168d20b23130812f6b950c
EkaitzLeKaitz <sagonzal@Santi>
2024-03-24 18:21:04 +0100

    COMPLETED PACO

--- ft_strchr.c
+++ ft_strchr.c
@@ -29,12 +29,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c)
-			i++;
-		else
+		if (s[i] == (char)c)
 			return ((char *)&s[i]);
+		i++;
 	}
-	if (c == '\0')
+	if ((char)c == '\0')
 		return ((char *)&s[i]);
 	return (0);
 }
@@ -44,8 +43,8 @@ int	main(void)
 	const char	*s;
 	int		c;
 
-	s = "comion";
-	c = 'o';
+	s = "teste";
+	c = '\0';
 //	printf ("Original: %s\n", strchr(s, c));
 	printf ("Propia: %s\n", ft_strchr(s, c));
 	return (0);
